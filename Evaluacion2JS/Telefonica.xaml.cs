e6a5acee919f55fa7b28e67dc885aa317cb5aee4
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-06-06 08:49:43 -0500

    Trate de poner una imagen y se ve feo

--- Evaluacion2JS/Telefonica.xaml.cs
+++ Evaluacion2JS/Telefonica.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Evaluacion2JS;
 
 public partial class Telefonica : ContentPage
@@ -5,12 +7,33 @@ public partial class Telefonica : ContentPage
     public Telefonica()
     {
         InitializeComponent();
+
+       
+
+
+
     }
+    private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        MaximumHeightRequest = 10;
+        Console.WriteLine($"Texto ingresado: {entryTelefono.Text}");
+    }
+
 
 
     async void JSButton_Clicked (object sender, EventArgs args)
     {
+        
         await label.RelRotateTo(360, 1000);
+        bool answer = await DisplayAlert("Confirmacion", "Seguro quieres recargar te vamos a estafabar veras", "Si, Me estan Amenzando", "No");
+        Debug.WriteLine("Answer: " + answer);
         await DisplayAlert("Muy bien!", "Su recarga se completo", "OK");
     }
+
+    Image image = new Image
+    {
+        Source = ImageSource.FromFile("conejito.jpg")
+    };
+
+
 }
