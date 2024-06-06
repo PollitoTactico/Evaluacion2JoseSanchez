df92b294b9ae6fa3b4f3d48c567f2411ac8ad0da
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-06-06 07:18:36 -0500

    Agregar archivos de proyecto.

---
+++ Evaluacion2JS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Evaluacion2JS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Evaluacion2JS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Evaluacion2JS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
