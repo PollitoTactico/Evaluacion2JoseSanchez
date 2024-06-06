3a88132213ceab30fac25fe75591d50b0c6608be
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-06-06 09:06:19 -0500

    El telefono no se ve bien pero es una ranura abajo solo acepta 10 numeros y no funciono el crear un documento le trate de generar y no hacerle con ruta

--- Evaluacion2JS/Telefonica.xaml.cs
+++ Evaluacion2JS/Telefonica.xaml.cs
@@ -1,39 +1,71 @@
 using System.Diagnostics;
+using System.IO;
 
-namespace Evaluacion2JS;
-
-public partial class Telefonica : ContentPage
+namespace Evaluacion2JS
 {
-    public Telefonica()
+    public partial class Telefonica : ContentPage
     {
-        InitializeComponent();
+        public Telefonica()
+        {
+            InitializeComponent();
+        }
 
-       
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            MaximumHeightRequest = 10;
+            Console.WriteLine($"Texto ingresado: {entryTelefono.Text}");
+        }
 
+        async void JSButton_Clicked(object sender, EventArgs args)
+        {
+            
+            await label.RelRotateTo(360, 1000);
+         
+            bool answer = await DisplayAlert("Confirmacion", "Seguro quieres recargar te vamos a estafabar veras", "Si, Me estan Amenzando", "No");
+            Debug.WriteLine("Answer: " + answer);
 
+            if (answer)
+            {
+                
+                await DisplayAlert("Muy bien!", "Su recarga se completo", "OK");
+                string numeroTelefono = entryTelefono.Text;
+                if (string.IsNullOrEmpty(numeroTelefono))
+                {
+                    await DisplayAlert("Error", "Por favor ingrese un número de teléfono válido.", "OK");
+                    return;
+                }
 
+                
+                string montoRecarga = "10";
+                if (Content is StackLayout stackLayout)
+                {
+                    foreach (var view in stackLayout.Children)
+                    {
+                        if (view is RadioButton radioButton && radioButton.IsChecked)
+                        {
+                            montoRecarga = radioButton.Content.ToString().Replace("$", "");
+                            break;
+                        }
+                    }
+                }
+             
+
+                string fechaActual = DateTime.Now.ToString("dd/MM/yyyy");
+
+          
+                string textoArchivo = $"Se hizo una recarga de {montoRecarga} dólares en la siguiente fecha; {fechaActual}";
+                string rutaArchivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{numeroTelefono}.txt");
+
+               
+                File.WriteAllText(rutaArchivo, textoArchivo);
+
+                Debug.WriteLine($"Archivo guardado en: {rutaArchivo}");
+            }
+        }
+
+        Image image = new Image
+        {
+            Source = ImageSource.FromFile("conejito.jpg")
+        };
     }
-    private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
-    {
-        MaximumHeightRequest = 10;
-        Console.WriteLine($"Texto ingresado: {entryTelefono.Text}");
-    }
-
-
-
-    async void JSButton_Clicked (object sender, EventArgs args)
-    {
-        
-        await label.RelRotateTo(360, 1000);
-        bool answer = await DisplayAlert("Confirmacion", "Seguro quieres recargar te vamos a estafabar veras", "Si, Me estan Amenzando", "No");
-        Debug.WriteLine("Answer: " + answer);
-        await DisplayAlert("Muy bien!", "Su recarga se completo", "OK");
-    }
-
-    Image image = new Image
-    {
-        Source = ImageSource.FromFile("conejito.jpg")
-    };
-
-
 }
